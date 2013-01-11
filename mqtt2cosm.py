4fdc2e53639b0dececbcaff138021b51d3b20d06
Xose Pérez <xose.perez@gmail.com>
2013-01-12 00:19:32 +0100

    Documentation and licensing

--- mqtt2cosm.py
+++ mqtt2cosm.py
@@ -1,12 +1,29 @@
 #! /usr/bin/python
-# -*- utf-8 -*-
+# -*- coding: utf-8 -*-
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
 
-__app__ = "MQTT to COSM gateway"
-__version__ = '0.1'
-__author__ = "Xose Perez"
-__copyright__ = "Copyright (C) Xose Perez"
-__license__ = 'TBD'
+#   MQTT to Cosm.com
+#   Copyright (C) 2013 by Xose Pérez
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+__app__ = "MQTT to Cosm.com"
+__version__ = "0.1"
+__author__ = "Xose Pérez"
+__contact__ = "xose.perez@gmail.com"
+__copyright__ = "Copyright (C) 2013 Xose Pérez"
+__license__ = 'GPL v3'
 
 import sys
 import time
@@ -19,6 +36,10 @@ from libs.Mosquitto import Mosquitto
 from libs.Cosm import Cosm
 
 class MQTT2Cosm(Daemon):
+    """
+    MQTT2Cosm daemon.
+    Glues the different components together
+    """
 
     debug = True
     mqtt = None
@@ -26,23 +47,36 @@ class MQTT2Cosm(Daemon):
     feeds = {}
 
     def log(self, message):
+        """
+        Log method.
+        TODO: replace with standard python logging facility
+        """
         if self.debug:
             timestamp = datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f')
             sys.stdout.write("[%s] %s\n" % (timestamp, message))
             sys.stdout.flush()
 
     def load(self, feeds):
+        """
+        Loads the mapping from MQTT topics to Cosm.com feeds
+        """
         self.feeds = {}
         for feed, datastreams in feeds.iteritems():
             for datastream, topic in datastreams.iteritems():
                 self.feeds[topic] = {'feed': feed, 'datastream': datastream}
 
     def cleanup(self):
+        """
+        Clean up connections and unbind ports
+        """
         self.mqtt.disconnect()
         self.log("[INFO] Exiting")
         sys.exit()
 
     def mqtt_connect(self):
+        """
+        Initiate connection to MQTT broker and bind callback methods
+        """
         self.mqtt.on_connect = self.mqtt_on_connect
         self.mqtt.on_disconnect = self.mqtt_on_disconnect
         self.mqtt.on_message = self.mqtt_on_message
@@ -50,8 +84,11 @@ class MQTT2Cosm(Daemon):
         self.mqtt.connect()
 
     def mqtt_on_connect(self, obj, result_code):
+        """
+        Callback when connection to the MQTT broker has succedeed or failed
+        """
         if result_code == 0:
-            self.log("[INFO] Connected to local MQTT broker")
+            self.log("[INFO] Connected to MQTT broker")
             self.mqtt.send_connected()
             for topic, feed in self.feeds.iteritems():
                 self.log("[DEBUG] Subscribing to %s" % topic)
@@ -60,14 +97,23 @@ class MQTT2Cosm(Daemon):
             self.stop()
 
     def mqtt_on_disconnect(self, obj, result_code):
+        """
+        Callback when disconnecting from the MQTT broker
+        """
         if result_code != 0:
             time.sleep(3)
             self.mqtt_connect()
 
     def mqtt_on_subscribe(self, obj, mid, qos_list):
-        self.log("Subscription with mid %s received." % mid)
+        """
+        Callback when succeeded ubscription
+        """
+        self.log("[INFO] Subscription with mid %s received." % mid)
 
     def mqtt_on_message(self, obj, msg):
+        """
+        Incoming message, publish to Coms.com if there is a mapping match
+        """
         feed = self.feeds.get(msg.topic, None)
         if feed:
             try:
@@ -78,7 +124,9 @@ class MQTT2Cosm(Daemon):
             cosm.push(feed['feed'], feed['datastream'], message)
 
     def run(self):
-
+        """
+        Entry point, initiates components and loops forever...
+        """
         self.log("[INFO] Starting " + __app__ + " v" + __version__)
         self.mqtt_connect()
 
