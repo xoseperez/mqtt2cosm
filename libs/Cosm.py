1f6272f7d069f70bd7298a98a04d71a4cb3d7a9b
Xose Pérez <xose.perez@gmail.com>
2013-01-12 00:30:22 +0100

    Added missing documentation to cosm.com class

--- libs/Cosm.py
+++ libs/Cosm.py
@@ -1,6 +1,26 @@
 #! /usr/bin/python
-# -*- utf-8 -*-
+# -*- coding: utf-8 -*-
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+
+#   Copyright (C) 2013 by Xose Pérez
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+__author__ = "Xose Pérez"
+__contact__ = "xose.perez@gmail.com"
+__copyright__ = "Copyright (C) 2013 Xose Pérez"
+__license__ = 'GPL v3'
 
 import urllib2
 import json
@@ -15,9 +35,15 @@ class Cosm(object):
     base_url = "http://api.cosm.com/v2/feeds/%s/datastreams/%s"
 
     def __init__(self, api_key):
+        """
+        Constructor, provide API Key
+        """
         self.api_key = api_key
 
     def _send(self, method, url, data = None):
+        """
+        Private method that performs the HTTP request
+        """
 
         output = True
 
@@ -34,18 +60,36 @@ class Cosm(object):
         return output
 
     def clear(self):
+        """
+        Reset the stored datapoints
+        """
         self.datapoints = []
 
     def add(self, at, value):
+        """
+        Add a datapoint with a given timestamp and value
+        """
         self.datapoints.append({'at': at, 'value': value})
 
     def send(self, feed, datastream):
+        """
+        Send all stored datapoints.
+        Usage example:
+            cosm = Cosm('APIKEY')
+            cosm.add('2012-12-27T12:00:00+01:00', 190)
+            cosm.add('2012-12-27T13:00:00+01:00', 150)
+            cosm.add('2012-12-27T14:00:00+01:00', 270)
+            cosm.send(94234, 1)
+        """
         url = self.base_url % (feed, datastream) + "/datapoints.json"
         data = json.dumps({'datapoints' : self.datapoints})
         response = self._send('POST', url, data)
         return response
 
     def get(self, feed, datastream, start, end, step=360):
+        """
+        Gets all datapoints (interval 0) from a feed between two dates
+        """
 
         ts_start = start
         step = datetime.timedelta(minutes=step)
@@ -65,6 +109,9 @@ class Cosm(object):
 
 
     def push(self, feed, datastream, value):
+        """
+        Pushes a single value with current timestamp to the given feed/datastream
+        """
         url = self.base_url % (feed, datastream) + ".json"
         data = json.dumps({'current_value' : value})
         return self._send('PUT', url, data)
